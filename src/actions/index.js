a673f77dd1cb5f7d9f4a05374d14e4115d9f0545
Yuehan Duan <yuehanduan@yahoo.com>
2018-05-05 16:42:56 -0700

    Add link from react router for page navigation.

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,6 +7,7 @@ const API_KEY = "?key=lamborghiniaventadorLP700-4";
 
 export function fetchPosts() {
   const request = axios.get(`${rootURL}/posts${API_KEY}`);
+
   return {
     type: FETCH_POSTS,
     payload: request,
--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,9 +1,41 @@
 import React, { Component } from "react";
+import _ from "lodash";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { fetchPosts } from "../actions";
 
 class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      );
+    });
+  }
+
   render() {
-    return <div>Posts Index</div>;
+    return (
+      <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a Post
+          </Link>
+        </div>
+        <h3>Posts</h3>
+        <ul className="list-group">{this.renderPosts()}</ul>
+      </div>
+    );
   }
 }
 
-export default PostsIndex;
+function mapStateToProps({ posts }) {
+  return { posts };
+}
+
+export default connect(null, { fetchPosts })(PostsIndex); //same as mapDispatchToProps, connects is taking care of it for us here
