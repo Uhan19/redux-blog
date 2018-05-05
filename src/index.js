3c5c82e4feb7a1a9ead7e47aeca161d5f02ddacb
Yuehan Duan <yuehanduan@yahoo.com>
2018-05-05 15:08:41 -0700

    Add browserrouter.
    fix react-router-dom dependency by upgrading to 4.2.2.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import App from './components/app';
-import reducers from './reducers';
+import App from "./components/app";
+import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Hello extends React.Component {
+  render() {
+    return <div>Hello!</div>;
+  }
+}
+
+class GoodBye extends React.Component {
+  render() {
+    return <div>GoodBye!</div>;
+  }
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <Router>
+      <div>
+        <Route path="/hello" component={Hello} />
+        <Route path="/goodbye" component={GoodBye} />
+      </div>
+    </Router>
+  </Provider>,
+  document.querySelector(".container")
+);
