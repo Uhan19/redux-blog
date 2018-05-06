9536de72ba312e14508a7af668c667534bd32f43
Yuehan Duan <yuehanduan@yahoo.com>
2018-05-05 17:20:39 -0700

    Add forms validation.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,9 +1,58 @@
 import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
 
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input type="text" {...field.input} />
+        {field.meta.error}
+      </div>
+    );
+  }
+
   render() {
-    return <div>PostsNew!</div>;
+    return (
+      <form>
+        <Field
+          label="Title For Post"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+      </form>
+    );
   }
 }
 
-export default PostsNew;
+function validate(values) {
+  const errors = {};
+  if (!values.title || values.title.length < 3) {
+    errors.title = "Enter a title that is at least 3 characters!";
+  }
+  if (!values.categories) {
+    errors.categories = "Enter some categories!";
+  }
+  if (!values.content) {
+    errors.content = "Enter some content!";
+  }
+
+  // if errors is empy, the form is fine to submit
+  // if errors has *any* properties, redux form assumes form is invalid
+  return errors;
+}
+
+export default reduxForm({
+  validate, //same as validate: validate
+  form: "PostsNewForm", //value has to be unique
+})(PostsNew);
